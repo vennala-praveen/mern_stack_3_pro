de08516b27d75fb2708ca2274ab0c5b754cb5045
Praveen Vennala <praveen98vennala@gmail.com>
2026-04-16 11:35:16 +0530

    title change & add new workout

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { FaDumbbell } from "react-icons/fa"; // Font Awesome Dumbbell icon
+import { FaDumbbell, FaRunning } from "react-icons/fa"; // Font Awesome Dumbbell icon
 
 const NavBar = () => {
   return (
@@ -9,7 +9,7 @@ const NavBar = () => {
           <h1>
             <FaDumbbell style={{ marginRight: "10px" }} />
             Gym Workout
-            <FaDumbbell style={{ marginLeft: "10px" }} />
+            <FaRunning style={{ marginLeft: "10px" }} />
           </h1>
         </Link>
       </div>
