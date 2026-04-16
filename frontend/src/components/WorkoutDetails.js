1eb06f51ffc1f323d3a6f4fbdb95e76bd2c65553
Praveen Vennala <praveen98vennala@gmail.com>
2026-04-16 11:57:35 +0530

    add new workout & delete button

--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -1,12 +1,28 @@
 import React from 'react'
+import { useWorkoutContext } from '../hooks/useWorkoutContext'
 
 const WorkoutDetails = ({workout}) => {
+
+  const {dispatch} = useWorkoutContext()
+
+  const handleClick =  async () => {
+    const response = await fetch('api/workouts/' + workout._id, {
+      method: 'DELETE'
+    })
+    const json = await response.json()
+
+    if(response.ok){
+      dispatch({ type: 'DELETE_WORKOUT', payload: json })
+    }
+  }
+
   return (
     <div className='workout-details'>
         <h4>{workout.title}</h4>
         <p><strong>Reps: </strong>{workout.reps}</p>
         <p><strong>Load (kg): </strong>{workout.load}</p>
         <p>{workout.createdAt}</p>
+        <span onClick={handleClick}>delete</span>
 
     </div>
   )
