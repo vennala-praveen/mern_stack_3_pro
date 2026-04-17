44574a33a155c7af5d91369aadbbeee82b17ce9c
Praveen Vennala <praveen98vennala@gmail.com>
2026-04-17 10:52:38 +0530

    added navbar & css

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -5,12 +5,13 @@ const NavBar = () => {
   return (
     <header>
       <div className="container">
-        <Link to="/">
+        <Link to="/" className="logo">
           <h1>
             <FaDumbbell style={{ marginRight: "10px" }} />
             Gym Workout
             <FaRunning style={{ marginLeft: "10px" }} />
           </h1>
+          <img src="https://thumbs.dreamstime.com/b/art-illustration-351215485.jpg" alt="" className="header-img"/>
         </Link>
       </div>
     </header>
