5168082e31a78088d81eef62f777294a2b09a921
Praveen Vennala <praveen98vennala@gmail.com>
2026-04-20 14:22:06 +0530

    added css & navbar

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -18,7 +18,7 @@ const NavBar = () => {
       </div>
 
       <div className="mqeen">
-          <p>Welcome to Gym Workout – Stay Fit, Stay Strong! NO pain No gain</p>
+          <p>Welcome to Gym Workout – Stay Fit, Stay Strong!, NO pain No gain</p>
       </div>
     </header>
   );
